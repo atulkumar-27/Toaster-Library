01676bbb3d7c7c254877e20877b651a96b243e23
Atul Kumar <atulku@zeta.tech>
2022-07-19 00:24:07 +0530

    java to koltin

---
+++ ToasterLibrary/src/main/java/com/atulkumar/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package com.atulkumar.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+object ToasterMessage {
+    fun showMessage(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
